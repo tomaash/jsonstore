83dae7e09fc1a8589199264d1bf3f4b3ccba32d7
Tomáš Holas <tomas.holas@gmail.com>
2018-11-01 12:24:28 +0100

    use compression

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var cors = require('cors')
+var compression = require('compression')
+
 var app = express()
 
 app.use(cors())
-
+app.use(compression())
 app.use(express.static('.'))
 
-// app.get('/products/:id', function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for all origins!'})
-// })
+const port = process.env.PORT || 4444
 
-app.listen(process.env.PORT, function () {
-  console.log('CORS-enabled web server listening on port ' + process.env.PORT)
+app.listen(port, function () {
+  console.log('CORS-enabled web server listening on port ' + port)
 })
